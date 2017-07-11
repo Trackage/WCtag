dbb3a6f103b1855815dcc80a3aed8979e90931b9
Michael Sumner <mdsumner@gmail.com>
2017-07-11 15:14:38 +1000

    Update wc_netcdf.R

--- R/wc_netcdf.R
+++ R/wc_netcdf.R
@@ -3,15 +3,7 @@
 # d <- read_csv("file.csv", skip = 4)
 # d$gmt <- as.POSIXct(strptime(d$Date, "%d-%b-%Y %H:%M:%S"), tz = "GMT")
 # library(tidync)
- 
-# read_wc_nc <- function(file) {
-#   lon <- (tidync::tidync(file) %>% activate("longitude") %>% hyper_slice())[[1]]
-#   lat <- rev((tidync::tidync(file) %>% activate("latitude") %>% hyper_slice())[[1]])
-#    a <- (tidync("42951-3.nc") %>% hyper_filter() %>% hyper_slice())[[1]]
-#   
-#    raster::setExtent(raster::brick(a, crs = "+proj=longlat +datum=WGS84", transpose = TRUE), raster::extent(range(lon), range(lat)))
-# }
-# 
+
 # b <- read_wc_nc("file.nc")
 # b2 <- disaggregate(b, fact = 8, method = "bilinear")
 # for (i in seq_len(nrow(d))) {
